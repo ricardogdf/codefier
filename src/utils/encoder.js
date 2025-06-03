a0b5c835349e3f1578fdca2f39c639ba64546277
ricardogdf <ricardogf2004@gmail.com>
2025-06-03 11:22:22 -0300

    Adicionando mais camadas de codificação

--- src/utils/encoder.js
+++ src/utils/encoder.js
@@ -1,3 +1,6 @@
+import AES from "crypto-js/aes";
+import Utf8 from "crypto-js/enc-utf8";
+
 const data = new Date(); // Replace with your date
 const timestamp =
   data.getDay() -
@@ -9,16 +12,43 @@ const timestamp =
 
 export function customEncode(text) {
   const shifted = text.split("").map((c) => c.charCodeAt(0) + timestamp);
-  return btoa(shifted.join(","));
+
+  const encoded = btoa(shifted.join(","))
+    .split("")
+    .map((char, i) => {
+      const charCode = char.charCodeAt(0);
+      const keyCode = timestamp
+        .toString()
+        .charCodeAt(i % timestamp.toString().length);
+      return (charCode ^ keyCode) + i; // XOR + deslocamento incremental
+    });
+
+  return AES.encrypt(
+    encoded.map((n) => n.toString(16)).join("-"),
+    timestamp.toString()
+  ).toString();
 }
 
 export function customDecode(encoded) {
   try {
-    const decoded = atob(encoded)
+    const bytes = AES.decrypt(encoded, timestamp.toString());
+
+    const parts = bytes
+      .toString(Utf8)
+      .split("-")
+      .map((hex) => parseInt(hex, 16));
+    const decoded = parts.map((num, i) => {
+      const keyCode = timestamp
+        .toString()
+        .charCodeAt(i % timestamp.toString().length);
+      return String.fromCharCode((num - i) ^ keyCode);
+    });
+    const encode = decoded.join("");
+
+    return atob(encode)
       .split(",")
       .map((n) => String.fromCharCode(Number(n) - timestamp))
       .join("");
-    return decoded;
   } catch {
     return "Erro ao decodificar.";
   }
